2e70a38bebfb2a16cc0ce484c199b5df3eba9844
Hoang Cao <cnhhoang850@gmail.com>
2020-08-23 13:40:12 +0700

    exercise 7b

--- routed-anecdotes/src/index.js
+++ routed-anecdotes/src/index.js
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { useState } from 'react'
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const reset = () => [
+        setValue('')
+    ]
+
+    return {
+        type,
+        value,
+        onChange,
+        reset
+    }
+}
 
 ReactDOM.render(
     <Router>
